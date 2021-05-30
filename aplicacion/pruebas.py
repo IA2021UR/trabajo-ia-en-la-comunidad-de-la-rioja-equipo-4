58e8d2adbc1eac44eb664adba2ba48935ec32a81
nemartal <43754337+nemartal@users.noreply.github.com>
2021-05-31 00:01:14 +0200

    aplicacion

--- aplicacion/pruebas.py
+++ aplicacion/pruebas.py
@@ -3,12 +3,13 @@ from tkinter import filedialog
 from PIL import Image
 from PIL import ImageTk
 import cv2
-import imutils
 import numpy as np
 
+from fastai import core
 
-from DeOldify.deoldify.visualize import *
+from deoldify.visualize import get_image_colorizer
 
+global numero
 def elegir_imagen():
     global path_image
     # Especificar los tipos de archivos, para elegir solo a las imágenes
@@ -22,10 +23,10 @@ def elegir_imagen():
 
         # Leer la imagen de entrada y la redimensionamos
         image = cv2.imread(path_image)
-        image= imutils.resize(image, height=380)
+        #image= imutils.resize(image, height=380)
         # Para visualizar la imagen de entrada en la GUI
-        imageToShow= imutils.resize(image, width=180)
-        imageToShow = cv2.cvtColor(imageToShow, cv2.COLOR_BGR2RGB)
+        #imageToShow= imutils.resize(image, width=180)
+        imageToShow = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         im = Image.fromarray(imageToShow )
         img = ImageTk.PhotoImage(image=im)
         lblInputImage.configure(image=img)
@@ -42,18 +43,20 @@ def elegir_imagen():
 def colorear_imagen():
     global image
 
+    numero = textbox.get(1.0, END)
+    numero = str(numero)
+    numero_str = numero.replace("\n", "")
+    print(numero_str)
+    print("ayuda")
+
     colorizer = get_image_colorizer(artistic=True)
-    render_factor = numero
+    render_factor = int(numero)
 
-    image_path = colorizer.plot_transformed_image_from_url(url=path_image, render_factor=render_factor,
-                                                           compare=True,
-                                                           watermarked=False)
+    image_path = colorizer.plot_transformed_image(path=path_image, render_factor=render_factor, compare=True, watermarked=False)
 
-
-    imageToShowOutput = cv2.cvtColor(image_path, cv2.COLOR_BGR2RGB)
+    im = Image.open(image_path)
     
     # Para visualizar la imagen en lblOutputImage en la GUI
-    im = Image.fromarray(imageToShowOutput)
     img = ImageTk.PhotoImage(image=im)
     lblOutputImage.configure(image=img)
     lblOutputImage.image = img
@@ -79,17 +82,18 @@ lblOutputImage.grid(column=1, row=1, rowspan=6)
 btn = Button(root, text="Elegir imagen", width=25, command=elegir_imagen)
 btn.grid(column=0, row=0, padx=5, pady=5)
 
-#Creamos el botón para colorear la imagen de entrada
-boton = Button(root, text="Colorear imagen", width=25, command=colorear_imagen)
-boton.grid(column=0,row=3, padx=5, pady=5)
-
 #Label para modificar el renderizado
 lblReder = Label(root, text="Elegir un numero entre 10 y 30")
-lblReder.grid(column=0, row=4)
+lblReder.grid(column=0, row=3)
 
 #Creamos el texto de entrada para obtener el renderizado
 textbox = Text(height=1, width=2);
-textbox.grid(column=0, row = 5)
-numero = textbox.get("1.0", END)
+textbox.grid(column=0, row = 4)
+
+#Creamos el botón para colorear la imagen de entrada
+boton = Button(root, text="Colorear imagen", width=25, command=colorear_imagen)
+boton.grid(column=0,row=5, padx=5, pady=5)
+
+
 root.mainloop()
 
